28e47925b00629fb3450408be8af6c87b2bd9ef9
Evelyne <e.umubyeyi@alustudent.com>
2023-03-26 11:47:13 +0200

    Added description in the index files of each folder in the src folder to describe what each folder will contain

---
+++ src/controllers/index.js
@@ -0,0 +1,10 @@
+// This is where controllers will be
+
+// tip:
+/**
+ *
+ * Controllers are responsible for handling
+ * incoming requests and returning responses to the client.
+ * A controller's purpose is to receive specific requests for the application.
+ *
+ */
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,3 +1,14 @@
+// This is where routers will be
+
+// tip:
+/**
+ *
+ * A route is a section of Express code that
+ * associates an HTTP verb ( GET , POST , PUT , DELETE , etc.),
+ * a URL path/pattern, and a function that is called to handle that pattern
+ *
+ */
+
 import express from 'express';
 
 const router = express.Router();
