db4bc545eac8ae43c8ea0bbee5cd529e47db4709
uwdjany <djanatiuwase@gmail.com>
2023-03-29 13:16:04 +0200

    setting environment variables and added dotenv into the project

---
+++ src/database/config/config.js
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv';
+dotenv.config();
+const config={
+  
+    development: {
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      dialect: "postgres"
+    },
+    test: {
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database:  process.env.DATABASE,
+      host: process.env.DATABASE_HOST,
+      dialect: "postgres"
+    },
+    production: {
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      host: process.env.DATABASE_HOST,
+      dialect: "postgres"
+    }
+  
+  
+
+}
+
+
+module.exports=config;
+
+
+
+
+
+
+
+
+
+
