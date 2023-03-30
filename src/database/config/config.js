f3ac8c8e5f3e779204f7a9a3b3ea43053a80eb84
Niyobuzima <54801429+Niyobuzima@users.noreply.github.com>
2023-03-30 17:29:48 +0200

    fixed error in git-actions yml file
    chore(cross-env)setup cross-env
     update script running

---
+++ src/database/config/config.js
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+dotenv.config();
+const config={
+    development: {
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      dialect: "postgres"
+    },
+    test: {
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database:  process.env.DATABASE,
+      host: process.env.DATABASE_HOST,
+      dialect: "postgres"
+    },
+    production: {
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      host: process.env.DATABASE_HOST,
+      dialect: "postgres"
+    }
+  
+}
+export default config;
