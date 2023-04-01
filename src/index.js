b7a0911d3268a1c31a6f2acbba84c39d442d8712
honore123 <imanishimwehono@gmail.com>
2023-04-01 16:44:16 +0200

    fix(resolve): branch conflicts

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import {Sequelize} from 'sequelize'
-import i18next from 'i18next';
-import Backend from 'i18next-fs-backend';
-import i18nextMiddleware from 'i18next-http-middleware';
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import router from './routes/routes';
-import options from './docs/apidoc';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { Sequelize } from "sequelize";
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+import i18nextMiddleware from "i18next-http-middleware";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import router from "./routes/routes";
+import options from "./docs/apidoc";
 
 i18next
   .use(Backend)
   .use(i18nextMiddleware.LanguageDetector)
   .init({
     backend: {
-      loadPath: './src/locales/{{lng}}/{{ns}}.json',
+      loadPath: "./src/locales/{{lng}}/{{ns}}.json",
     },
-    fallbackLng: 'en',
-    preload: ['en', 'fr'],
+    fallbackLng: "en",
+    preload: ["en", "fr"],
   });
 
 const app = express();
@@ -42,9 +42,11 @@ app.use(express.json());
 
 const specs = swaggerJSDoc(options);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-app.get('/', (req, res) => res.status(200).json({ status: 200, message: req.t('welcome_message') }));
+app.get("/", (req, res) =>
+  res.status(200).json({ status: 200, message: req.t("welcome_message") })
+);
 
 app.use(router);
 
