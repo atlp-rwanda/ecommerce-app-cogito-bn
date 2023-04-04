2544c69d876e3c0ffb8fba22158e1b6bc858d3b3
honore123 <imanishimwehono@gmail.com>
2023-04-04 21:15:52 +0200

    fix(rename): rename database name database based on running environment [done #26]

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { Sequelize } from "sequelize";
-import i18next from "i18next";
-import Backend from "i18next-fs-backend";
-import i18nextMiddleware from "i18next-http-middleware";
-import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
-import router from "./routes/routes";
-import options from "./docs/apidoc";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import i18next from 'i18next';
+import Backend from 'i18next-fs-backend';
+import i18nextMiddleware from 'i18next-http-middleware';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import router from './routes/routes';
+import options from './docs/apidoc';
 
 i18next
   .use(Backend)
   .use(i18nextMiddleware.LanguageDetector)
   .init({
     backend: {
-      loadPath: "./src/locales/{{lng}}/{{ns}}.json",
+      loadPath: './src/locales/{{lng}}/{{ns}}.json',
     },
-    fallbackLng: "en",
-    preload: ["en", "fr"],
+    fallbackLng: 'en',
+    preload: ['en', 'fr'],
   });
 
 const app = express();
@@ -28,31 +27,19 @@ app.use(cors());
 
 dotenv.config();
 const port = process.env.PORT;
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: "postgres",
-    dialect: "postgres",
-  }
-);
 
 app.use(express.json());
 
 const specs = swaggerJSDoc(options);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-app.get("/", (req, res) =>
-  res.status(200).json({ status: 200, message: req.t("welcome_message") })
-);
+app.get('/', (req, res) => res.status(200).json({ status: 200, message: req.t('welcome_message') }));
 
 app.use(router);
 
 app.listen(port, async () => {
-  await sequelize.authenticate();
-  console.log("Database Connected!");
+  console.log('Database Connected!');
   console.log(`app listening on port ${port}`, process.env.NODE_ENV);
 });
 
