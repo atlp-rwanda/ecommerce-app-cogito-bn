58f0cc0a0fc1e8d4e8ab99663de0c1731c0868a3
Evelyne <e.umubyeyi@alustudent.com>
2023-04-04 15:09:27 +0200

    feat(#BN-9: Two factor authentication for vendors): I added sending OTP message to the user upon successful registration using twilio

--- src/database/seeders/20230326221727-demo-cogito.js
+++ src/database/seeders/20230326221727-demo-cogito.js
@@ -1,93 +1,145 @@
-'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          orders_id: 1,
+          wishlists_id: 1,
+          carts_id: 1,
+          firstName: 'Agnes',
+          lastName: 'Kunda',
+          email: 'kundaaggy@gmail.com',
+          password: 'kunda123',
+          role: 'admin',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
 
-  await queryInterface.bulkInsert('User', [{
-      orders_id: 1,
-      wishlists_id:1,
-      carts_id:1,
-      firstName: "Agnes",
-      lastName: "Kunda",
-      email: "kundaaggy@gmail.com",
-      password: "kunda123",
-      phone: "+250780162353",
-      role: "admin",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }],
-  
-      {});
+      {},
+    );
 
-      await queryInterface.bulkInsert('carts', [{
-        user_id: "1",
-        product_id: "5",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }],
-        {});
-      await queryInterface.bulkInsert('products', [{
-        name: "Laptop",
-        description: "MacBook Pro",
-        category_id: "1",
-        vendor_id: "1",
-        image: "image.png",
-        price: "600$",
-        quantity: "100",
-        stock: "In Stock",
-        carts_id: 1,
-        orders_id: 2,
-        wishlists_id:2,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }],
-        {});
-        await queryInterface.bulkInsert('wishlists', [{
-          name: "Laptop",
+    await queryInterface.bulkInsert(
+      'carts',
+      [
+        {
+          user_id: '1',
+          product_id: '5',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+    await queryInterface.bulkInsert(
+      'products',
+      [
+        {
+          name: 'Laptop',
+          description: 'MacBook Pro',
+          category_id: '1',
+          vendor_id: '1',
+          image: 'image.png',
+          price: '600$',
+          quantity: '100',
+          stock: 'In Stock',
+          carts_id: 1,
+          orders_id: 2,
+          wishlists_id: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+    await queryInterface.bulkInsert(
+      'wishlists',
+      [
+        {
+          name: 'Laptop',
           seller_id: 1,
           user_id: 2,
           product_id: 3,
-          description: "MacBook Pro",
-          image: "image.png",
-          price: "600$",
-          quantity: "100",
-          totalPrice: "60000$",
-          stock: "In Stock",
-          Expire_Date: "2035-03-27 ",
+          description: 'MacBook Pro',
+          image: 'image.png',
+          price: '600$',
+          quantity: '100',
+          totalPrice: '60000$',
+          stock: 'In Stock',
+          Expire_Date: '2035-03-27 ',
           createdAt: new Date(),
-          updatedAt: new Date()
-        }],
-          {}); 
-          await queryInterface.bulkInsert('categories', [{
-            name: "Laptop",
-            product_id: 4,
-            createdAt: new Date(),
-            updatedAt: new Date()
-          }],
-            {}); 
-            await queryInterface.bulkInsert('orders', 
-            [{
-              user_id: 1,
-              product_id:1,
-              quantity: 50,
-              status: "Picking on Site",
-              createdAt: new Date(),
-              updatedAt: new Date()
-            }],
-           
-              {}); 
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+    await queryInterface.bulkInsert(
+      'categories',
+      [
+        {
+          name: 'Laptop',
+          product_id: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+    await queryInterface.bulkInsert(
+      'orders',
+      [
+        {
+          user_id: 1,
+          product_id: 1,
+          quantity: 50,
+          status: 'Picking on Site',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+
+      {},
+    );
+    await queryInterface.bulkInsert(
+      'vendors',
+      [
+        {
+          id: '1',
+          fullName: 'NDAHAYO Bertin',
+          email: 'ndahayosibertin17@gmail.com',
+          password: 'NDABer123',
+          phoneNumber: '+250786949188',
+          businessName: 'ITH',
+          businessAddress: 'KN 48B ST',
+          businessPhoneNumber: '+250781346188',
+          businessEmail: 'ith.querries@gmail.com',
+          businessWebsite: 'www.ith.com',
+          businessDescription:
+            'We are the Number One Wholesale company of all IT related product in Rwanda',
+          businessLogo: 'https://www.pexels.com/photo/photo-of-computers-near-windows-3747481/',
+          productCategories: 'Computers, Phones, Printers, Phones, Accessories',
+          paymentMethods: 'MoMo, PayPal, VISA',
+          status: 'ACTIVE',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+
+      {},
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     // Add commands to revert seed here.
-   
-  await queryInterface.bulkDelete('User', null, {});
-  await queryInterface.bulkDelete('carts', null, {});
-  await queryInterface.bulkDelete('products', null, {});
-  await queryInterface.bulkDelete('wishlists', null, {});
-  await queryInterface.bulkDelete('categories', null, {});
-  await queryInterface.bulkDelete('orders', null, {});
-     
-  }
+
+    await queryInterface.bulkDelete('users', null, {});
+    await queryInterface.bulkDelete('carts', null, {});
+    await queryInterface.bulkDelete('products', null, {});
+    await queryInterface.bulkDelete('wishlists', null, {});
+    await queryInterface.bulkDelete('categories', null, {});
+    await queryInterface.bulkDelete('orders', null, {});
+    await queryInterface.bulkDelete('vendors', null, {});
+  },
 };
