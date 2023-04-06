e30150e2264b40b33c45015dd6cd17c73aa26382
Illustre13 <ndahayosibertin17@gmail.com>
2023-04-06 15:19:40 +0200

    worked on input validation
    encrypting vendors password
    
    login of the vendors
    
    providing valid token upon sucessful login

--- src/controllers/vendorsController.js
+++ src/controllers/vendorsController.js
@@ -1,5 +1,9 @@
 const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt');
 const { vendors } = require('../database/models');
+const { validateVendorRegistration } = require('../middleware/vendor/registerVendorValidator');
+const { validateVendorLogin } = require('../middleware/vendor/registerVendorValidator');
+const { vendorSignAccessToken } = require('../middleware/vendor/registerVendorValidator');
 
 const getAllVendors = async (req, res) => {
   try {
@@ -11,9 +15,39 @@ const getAllVendors = async (req, res) => {
   }
 };
 
+// eslint-disable-next-line consistent-return
 const registerVendor = async (req, res) => {
   try {
-    const vendor = await vendors.create(req.body);
+    const { error } = validateVendorRegistration(req.body);
+    if (error) {
+      console.log(error);
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    const vendorEmail = await vendors.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (vendorEmail) {
+      return res.status(409).json({ message: 'Vendor with this email already exists' });
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newVendor = await vendors.create({
+      fullName: req.body.fullName,
+      email: req.body.email,
+      password: hashedPassword,
+      phoneNumber: req.body.phone,
+      businessName: req.body.businessName,
+      businessAddress: req.body.businessAddress,
+      businessPhoneNumber: req.body.businessPhone,
+      businessEmail: req.body.businessEmail,
+      businessWebsite: req.body.businessWebsite,
+      businessDescription: req.body.businessDescription,
+      businessLogo: req.body.businessLogo,
+      productCategories: req.body.productCategories,
+      paymentMethods: req.body.paymentMethods,
+      status: req.body.status,
+    });
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -23,22 +57,22 @@ const registerVendor = async (req, res) => {
     });
     const mailOptions = {
       from: 'ndahayosibertin17@gmail.com',
-      to: vendor.email,
+      to: newVendor.email,
       subject: 'Welcome to Cogito',
-      text: `Dear ${vendor.fullName},\n\nWelcome to Cogito! We are excited to have you as a new vendor. Your business information has been successfully saved in our system.\n\nHere is the information you provided:\n\nFull Name: ${vendor.fullName}\nBusiness Name: ${vendor.businessName}\nBusiness Address: ${vendor.businessAddress}\nBusiness Phone Number: ${vendor.businessPhoneNumber}\nBusiness Email: ${vendor.businessEmail}\nBusiness Website: ${vendor.businessWebsite}\nBusiness Description: ${vendor.businessDescription}\nProduct Categories: ${vendor.productCategories}\nPayment Methods: ${vendor.paymentMethods}\nRandom Password: ${vendor.password}, You can change this password only after login to our site.\n Login Here.\n\nThank you for choosing Cogito!\n\nBest regards,\nThe Cogito Team`,
+      text: `Dear ${newVendor.fullName},\n\nWelcome to Cogito! We are excited to have you as a new vendor. Your business information has been successfully saved in our system.\n\nHere is the information you provided:\n\nFull Name: ${newVendor.fullName}\nBusiness Name: ${newVendor.businessName}\nBusiness Address: ${newVendor.businessAddress}\nBusiness Phone Number: ${newVendor.businessPhoneNumber}\nBusiness Email: ${newVendor.businessEmail}\nBusiness Website: ${newVendor.businessWebsite}\nBusiness Description: ${newVendor.businessDescription}\nProduct Categories: ${newVendor.productCategories}\nPayment Methods: ${newVendor.paymentMethods}\nRandom Password: ${req.body.password}, You can change this password only after login to our site.\n Login Here.\n\nThank you for choosing Cogito!\n\nBest regards,\nThe Cogito Team`,
     };
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
+    transporter.sendMail(mailOptions, (mailError, info) => {
+      if (mailError) {
+        console.log(mailError);
         res.status(500).json('Error sending email');
       } else {
         console.log(`Email sent: ${info.response}`);
-        res.status(201).json(vendor);
+        res.status(201).json(newVendor);
       }
     });
   } catch (error) {
-    console.log(error.message);
-    res.status(500).json(error.message);
+    console.log(error);
+    res.status(500).json(error);
   }
 };
 
@@ -81,10 +115,41 @@ const deleteVendor = async (req, res) => {
     res.status(500).json(error.message);
   }
 };
+const vendorLogin = async (req, res, next) => {
+  const { email } = req.body;
+  const { password } = req.body;
+  try {
+    const { error } = validateVendorLogin(req.body);
+    if (error) {
+      console.log(error);
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    const vendor = await vendors.findOne({ where: { email } });
+    if (!vendor) {
+      return res.status(401).json({ message: 'Invalid email' });
+    }
+    const isMatch = await bcrypt.compare(password, vendor.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+    const vendorLoginToken = await vendorSignAccessToken(
+      vendor.id,
+      vendor.fullName,
+      vendor.email,
+      vendor.status,
+    );
+    res.status(200).json({ vendorLoginToken });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
 module.exports = {
   getAllVendors,
   registerVendor,
   findVendorByID,
   updateVendor,
   deleteVendor,
+  vendorLogin,
 };
---
+++ src/middleware/vendor/registerVendorValidator.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+
+const validator = (schema) => (payload) => schema.validate(payload, { abortEarly: false });
+
+const registerVendor = Joi.object({
+  fullName: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/).min(8).max(16)
+    .required(),
+  phoneNumber: Joi.string().pattern(/^\+?[0-9]{6,}$/).required(),
+  businessName: Joi.string().required(),
+  businessAddress: Joi.string().required(),
+  businessPhoneNumber: Joi.string().pattern(/^\+?[0-9]{6,}$/).required(),
+  businessEmail: Joi.string().email().required(),
+  businessWebsite: Joi.string().uri(),
+  businessDescription: Joi.string().required(),
+  businessLogo: Joi.any(),
+  productCategories: Joi.string().required(),
+  paymentMethods: Joi.string().required(),
+  status: Joi.string().required(),
+});
+
+const loginVendor = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/).min(8).max(16)
+    .required(),
+});
+exports.validateVendorRegistration = validator(registerVendor);
+exports.validateVendorLogin = validator(loginVendor);
