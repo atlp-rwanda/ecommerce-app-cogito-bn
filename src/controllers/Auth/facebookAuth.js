d44cb4823d99ff1a8e0319f3cdae1ba64b44f3f1
Andela Rwanda <andelarwanda@Andelas-MacBook-Pro.local>
2023-04-14 18:29:20 +0200

    fixed the issue in the response message and response file

--- src/controllers/Auth/facebookAuth.js
+++ src/controllers/Auth/facebookAuth.js
@@ -1,24 +1,29 @@
-import passport from 'passport';
-import { Strategy as FacebookStrategy } from 'passport-facebook';
-import {user} from "../../database/models"
-import 'dotenv/config'
-passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: '/auth/facebook/callback',
-  profileFields: ['id', 'displayName', 'email']
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    const [newUser] = await user.findOrCreate({
-      where: { email: profile.emails[0].value },
-      defaults: {
-        email: profile.emails[0].value,
-        name: profile.displayName
+import passport from "passport";
+import { Strategy as FacebookStrategy } from "passport-facebook";
+import { user } from "../../database/models";
+import "dotenv/config";
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: "/auth/facebook/callback",
+      profileFields: ["id", "displayName", "email"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const [newUser] = await user.findOrCreate({
+          where: { email: profile.emails[0].value },
+          defaults: {
+            email: profile.emails[0].value,
+            name: profile.displayName,
+          },
+        });
+        return done(null, newUser);
+      } catch (err) {
+        return done(err, null);
       }
-    });
-    return done(null, newUser);
-  } catch (err) {
-    return done(err, null);
-  }
-}));
+    }
+  )
+);
 export default passport;
--- src/controllers/Auth/signupController.js
+++ src/controllers/Auth/signupController.js
@@ -1,10 +1,10 @@
-import { user } from '../../database/models';
-import { generateConfirmationCode } from '../../utils/validation/generateCode';
-import nodemailer from 'nodemailer';
-import dotenv from 'dotenv';
-import { hashPassword } from '../../utils/validation/hashedPassword';
-import Response from '../../utils/response';
-import httpStatus from 'http-status-codes';
+import { user } from "../../database/models";
+import { generateConfirmationCode } from "../../utils/validation/generateCode";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import { hashPassword } from "../../utils/validation/hashedPassword";
+import Response from "../../utils/response";
+import httpStatus from "http-status";
 dotenv.config();
 const signUp = async (req, res) => {
   const { email, password } = req.body;
@@ -13,43 +13,43 @@ const signUp = async (req, res) => {
   if (!emailRegex.test(email)) {
     return Response.errorMessage(
       res,
-      'Invalid email address',
+      "Invalid email address",
       httpStatus.BAD_REQUEST
     );
   }
   if (!password || password.length < 8) {
     return Response.errorMessage(
       res,
-      'Password must be at least 8 characters long',
+      "Password must be at least 8 characters long",
       httpStatus.BAD_REQUEST
     );
   }
   try {
     const hashedPassword = await hashPassword(password);
-    const existingUser = await  user.findOne({ where: { email } });
+    const existingUser = await user.findOne({ where: { email } });
     if (existingUser) {
       return Response.errorMessage(
         res,
-        'Email already exists',
+        "Email already exists",
         httpStatus.BAD_REQUEST
       );
     }
-    const newUser = await  user.create({
+    const newUser = await user.create({
       email,
       password: hashedPassword,
       confirmationCode,
-      firstName: 'defaultFirstName',
-      lastName: 'defaultLastName',
-      role: 'buyer',
+      firstName: "defaultFirstName",
+      lastName: "defaultLastName",
+      role: "buyer",
     });
     const option = {
-      from: 'djanatiuwase@gmail.com',
+      from: "djanatiuwase@gmail.com",
       to: email,
-      subject: 'Confirm your account',
+      subject: "Confirm your account",
       html: `<p>Please click <a href="http://localhost:9999/user/confirm/${confirmationCode}">here</a> to confirm your account.</p>`,
     };
     const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
+      host: "smtp.gmail.com",
       port: 465,
       secure: true,
       auth: {
@@ -57,26 +57,57 @@ const signUp = async (req, res) => {
         pass: process.env.EMAIL_PASS,
       },
     });
-    transporter.sendMail(option, (error, info) => {
-      if (error) {
-        return Response.errorMessage(
+    // transporter.sendMail(option, (error, info) => {
+    //   if (error) {
+    //     return Response.errorMessage(
+    //       res,
+    //       'Failed to send confirmation email',
+    //       httpStatus.INTERNAL_SERVER_ERROR
+    //     );
+    //   }
+    //   console.log(`Email sent: ${email}`);
+
+    //   Response.successMessage(
+    //     res,
+    //     'Confirmation email sent to your email, check it and confirm your account to finish signing up',
+    //     httpStatus.OK
+    //   );
+    // });
+    const sendEmail = (option) => {
+      return new Promise((resolve, reject) => {
+        transporter.sendMail(option, (error, info) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(info);
+          }
+        });
+      });
+    };
+
+    (async () => {
+      try {
+        const info = await sendEmail(option);
+        console.log(`Email sent: ${email}`);
+        Response.successMessage(
           res,
-          'Failed to send confirmation email',
+          `Check the email sent to this ${email}, and confirm your account to complete the signup process.`,
+          httpStatus.OK
+        );
+      } catch (error) {
+        console.error(error);
+        Response.errorMessage(
+          res,
+          `Failed to send confirmation email: ${error.message}`,
           httpStatus.INTERNAL_SERVER_ERROR
         );
       }
-      console.log(`Email sent: ${email}`);
-      return Response.successMessage(
-        res,
-        'User created successfully',
-        httpStatus.OK
-      );
-    });
+    })();
   } catch (error) {
     console.log(error);
     return Response.errorMessage(
       res,
-      'Failed to create user',
+      "Failed to create user",
       httpStatus.INTERNAL_SERVER_ERROR
     );
   }
