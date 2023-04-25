ca82150bff6ba398f63edcb3b437b39a8bddd6b1
Illustre13 <ndahayosibertin17@gmail.com>
2023-04-25 12:06:35 +0200

    chore(maintain): reviewed my tasks
    [Finishes #5]

--- src/index.js
+++ src/index.js
@@ -6,61 +6,32 @@ import Backend from 'i18next-fs-backend';
 import i18nextMiddleware from 'i18next-http-middleware';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-<<<<<<< HEAD
 import cookieParser from 'cookie-parser';
 import userRouter from './routes/userrouters';
 import { sequelize } from './database/models';
-=======
 import bodyParser from 'body-parser';
->>>>>>> b0a7e53 (feat(vendor): register vendor functionalities)
 import router from './routes/routes';
 import profileRouter from './routes/profileRouter';
 import options from './docs/apidoc';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import signupRouter from './routes/user/userRoutes';
 import googleAuth from './routes/user/googleAuthRoutes';
 import facebookAuth from './routes/user/facebookAuthRoutes';
-=======
 import { vendors } from './database/models';
 
-<<<<<<< HEAD
 //const { Vendors } = require('./database/models');
->>>>>>> b0a7e53 (feat(vendor): register vendor functionalities)
-=======
-// const { Vendors } = require('./database/models');
->>>>>>> 02830ce (fixed update endpoint)
-=======
-import vendorRouter from './routes/vendorsRoutes';
->>>>>>> 891c0a1 (worked on sending email using nodemailer and gmail)
-=======
-import vendorRouter from './routes/vendor/vendorsRoutes';
-<<<<<<< HEAD
->>>>>>> 2da3d98 (done with the vendor api endpoints documentation using swagger)
-=======
-import userRouter from './routes/user/userRoutes';
->>>>>>> d6cc9fb (ft-vendor endpoint authorization)
 
 i18next
   .use(Backend)
   .use(i18nextMiddleware.LanguageDetector)
   .init({
     backend: {
-      loadPath: '././././././src/locales/{{lng}}/{{ns}}.json',
+      loadPath: './src/locales/{{lng}}/{{ns}}.json',
     },
     fallbackLng: 'en',
     preload: ['en', 'fr'],
   });
 const app = express();
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 app.use(express.urlencoded({extended: false}));
->>>>>>> b0a7e53 (feat(vendor): register vendor functionalities)
-=======
-app.use(express.urlencoded({ extended: false }));
->>>>>>> 02830ce (fixed update endpoint)
 app.use(i18nextMiddleware.handle(i18next));
 app.use(bodyParser.json());
 app.use(cors());
@@ -77,9 +48,7 @@ app.use(googleAuth);
 app.use(facebookAuth);
 
 app.get('/', (req, res) => res.status(200).json({ status: 200, message: req.t('welcome_message') }));
-<<<<<<< HEAD
 
-<<<<<<< HEAD
 app.use(userRouter);
 app.use('/profile', profileRouter);
 app.use(router);
@@ -91,71 +60,4 @@ app.listen(port, async () => {
   console.log('Database Connected!');
 });
 
-=======
-app.get('/vendors', async (req, res) => {
-  try {
-    const vendor = await vendors.findAll();
-    res.status(200).json(vendor);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json(error.message);
-  }
-});
-
-app.post('/vendors', async (req, res) => {
-  try {
-    const vendor = await vendors.create(req.body);
-    res.status(201).json(vendor);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json(error.message);
-  }
-});
-
-app.get('/vendors/:id', async (req, res) => {
-  try {
-    const vendor = await vendors.findByPk(req.params.id);
-    res.status(200).json(vendor);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json(error.message);
-  }
-});
-
-app.put('/vendors/:id', async (req, res) => {
-  try {
-    const vendor = await vendors.findByPk(req.params.id);
-    await vendor.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json(vendor);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json(error.message);
-  }
-});
-app.delete('/vendors/:id', async (req, res) => {
-  try {
-    const vendor = await vendors.findByPk(req.params.id);
-    await vendors.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json(vendor);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json(error.message);
-  }
-});
-
-=======
->>>>>>> 891c0a1 (worked on sending email using nodemailer and gmail)
-app.use(router);
-app.use('/vendors', vendorRouter);
-app.use('/users', userRouter);
-app.listen(port, () => console.log(`app listening on port ${port}`, process.env.NODE_ENV));
->>>>>>> b0a7e53 (feat(vendor): register vendor functionalities)
 export default app;
