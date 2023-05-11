89f3bbe3cc8a7f3672ba830083ea3bad405d5c63
Lydie Muhawenimana <84508034+Lydie260@users.noreply.github.com>
2023-05-11 16:34:18 +0200

    BN-16 feat: A seller should be able to delete a specific item (#54) (#33)
    * * feat(logout):create the logout feature
    -included user logout
    -documented the user logout
    [finishes #30]
    
    * feat(recommended product): worked on an algorithm to generate product recommendation to the buyer (#46)
    
    - created a function in recommendedProdcutController for a single product view which will save all the transaction in the of viewd prodcut by the buyer
    - created product recommendation algorithm that checks the buyer wishlist and viewed products and categories to generates the recommended products, it also include similar buyer preferences.
    - worked on the api swagger documentation of the above function
    - worked on the internationalization of responsesworked on test cases for the above function
    [Finishes #27]
    
    ---------
    
    Co-authored-by: Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
    Co-authored-by: Nelly KAWERA <78723561+nellykawera@users.noreply.github.com>
    Co-authored-by: Bertin NDAHAYO SINGIZWA <100227921+Illustre13@users.noreply.github.com>
    
    feat(update-password): endpoint of Updating your existing pasword and prompt of changing password
    -create passwordUpdateController.js
    - Create middleware passwordPrompt.js
    - tests for updatePassword and passwordPrompt
    -create bcypt.util.js
    -add middleware in login path route
    -swagger documentation for updatePassword
    [Finishe #11]

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,9 +1,9 @@
-import nodemailer from 'nodemailer';
-import HeaderComponent from './_email_/emailHeader';
-import FooterComponent from './_email_/emailFooter';
+import nodemailer from "nodemailer";
+import HeaderComponent from "./_email_/emailHeader";
+import FooterComponent from "./_email_/emailFooter";
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_ADDRESS,
     pass: process.env.EMAIL_PASSWORD,
@@ -14,9 +14,12 @@ const sendPasswordResetEmail = (to, userName, resetToken) => {
   const mailOptions = {
     from: process.env.EMAIL_ADDRESS,
     to,
-    subject: 'Password Reset',
+    subject: "Password Reset",
     html: `${HeaderComponent} <p>Dear ${userName}, </p><p>You are receiving this email because you (or someone else) have requested a password reset for your account.\n\n
-      Please click on the following link <a href="${process.env.RESET_URL.replace('${resetToken}', resetToken)}"> here </a> or paste it into your browser to complete the process:\n\n
+      Please click on the following link <a href="${process.env.RESET_URL.replace(
+        "${resetToken}",
+        resetToken
+      )}"> here </a> or paste it into your browser to complete the process:\n\n
       If you did not request this, please ignore this email and your password will remain unchanged.\n</p><p> ${FooterComponent}</p>`,
   };
 
