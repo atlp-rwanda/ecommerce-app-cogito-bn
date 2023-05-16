305805036d248d5e790fe5fc2088c31e6d46ad56
0784111172 <kaweranelly@gmail.com>
2023-05-16 23:17:30 +0300

    feat(buyer should receive an order notification) -Included the OrderConfirmationController.js -included orderServices.js -included urlUtils.js [finish #7]

--- src/controllers/orderConfirmationController.js
+++ src/controllers/orderConfirmationController.js
@@ -1,23 +1,21 @@
-import { orderServices, emailServices, urlUtils } from '../services';
+import { getOrderById } from '../services/orderServices';
+import sendEmail from '../services/emailServices';
+import urlUtils from '../services/urlUtils';
 
 const OrderConfirmationController = {
   sendConfirmationEmail: async (req, res) => {
     try {
+      console.log(req.body);
       const { orderId, buyerEmail } = req.body;
-
       // Get order details and tracking number
-      const order = await orderServices.getOrderById(orderId);
-      const trackingNumber = orderServices.generateTrackingNumber();
-
+      const order = await getOrderById(orderId); // Remove "orderServices."
+      const trackingNumber = generateTrackingNumber(); // Remove "orderServices."
       // Update the order with the tracking number
-      await orderServices.updateOrderTrackingNumber(orderId, trackingNumber);
-
+      await updateOrderTrackingNumber(orderId, trackingNumber); // Remove "orderServices."
       // Construct the confirmation email content
       const emailContent = constructConfirmationEmail(order, trackingNumber);
-
       // Send the confirmation email to the buyer's email address
-      await emailServices.sendEmail(buyerEmail, 'Order Confirmation', emailContent);
-
+      await sendEmail(buyerEmail, 'Order Confirmation', emailContent);
       return res.status(200).json({
         code: 200,
         message: 'Confirmation email sent successfully',
@@ -31,11 +29,9 @@ const OrderConfirmationController = {
     }
   },
 };
-
 // Helper function to construct the confirmation email content
 function constructConfirmationEmail(order, trackingNumber) {
   const orderStatusUrl = urlUtils.getOrderStatusUrl(order.id);
-
   return `
     <h1>Order Confirmation</h1>
     <p>Thank you for your order!</p>
@@ -48,5 +44,4 @@ function constructConfirmationEmail(order, trackingNumber) {
     <p>You can track the status of your order and view updates on the <a href="${orderStatusUrl}">Order Status Page</a>.</p>
   `;
 }
-
 export default OrderConfirmationController;
