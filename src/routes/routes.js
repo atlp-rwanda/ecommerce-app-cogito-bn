69c53173ae3734619589f17a10a3fc833b2a8211
0784111172 <kaweranelly@gmail.com>
2023-05-16 23:17:30 +0300

    feat(buyer should receive an order notification) -Included the OrderConfirmationController.js -included orderServices.js -included urlUtils.js [finish #7]

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,3 @@
-// tip:
-/**
- *
- * A route is a section of Express code that
- * associates an HTTP verb ( GET , POST , PUT , DELETE , etc.),
- * a URL path/pattern, and a function that is called to handle that pattern
- *
- */
 import express from 'express';
 import cors from 'cors';
 import i18next from 'i18next';
