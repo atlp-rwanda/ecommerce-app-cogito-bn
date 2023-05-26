ce0586726d1abe101b505d659d99bbf5fe53c067
Uwase Djanati <97160202+uwdjany@users.noreply.github.com>
2023-05-26 08:46:34 +0200

    feat(Notifications)Seller should be to receive notification (#75)
    -Added product Notification
    -email send to the vendor after product created
    [finishes #cp-3]

--- src/routes/user/userRoutes.js
+++ src/routes/user/userRoutes.js
@@ -3,6 +3,6 @@ import userAuthController from '../../controllers/Auth/signupController';
 import confirmationEmail from '../../controllers/Auth/confirmEmail';
 
 const router = express.Router();
-router.post('/signup', userAuthController.signUp);
+router.post('/buyer/signup', userAuthController.signUp);
 router.get('/confirm/:confirmationCode', confirmationEmail.confirmEmail);
 export default router;
